59964fd77a87789e6174ec6f95a1d05ae40d659c
Frans Johansson <fransjohansson98@gmail.com>
2021-12-27 21:29:25 +0100

    Fix loggers only working in one module at a time

--- src/code_query/utils/logging.py
+++ src/code_query/utils/logging.py
@@ -7,10 +7,12 @@ import logging.config
 from code_query.utils.serialize import yml_load
 
 
+log_config = yml_load("./config/logging.yml")
+logging.config.dictConfig(log_config)
+
+
 def get_logger(name: str) -> logging.Logger:
     """
     Returns a named logger
     """ 
-    log_config = yml_load("./config/logging.yml")
-    logging.config.dictConfig(log_config)
     return logging.getLogger(name)
