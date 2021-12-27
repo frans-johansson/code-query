7f3d8cbea1def9a2bc838f5214cb65d51daac08f
Frans Johansson <fransjohansson98@gmail.com>
2021-12-27 16:25:40 +0100

    Set up a private installable package for the code

---
+++ src/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='code_query',
+      version='0.1',
+      description='Internal private package for the code-query project',
+      author='Frans Johansson',
+      packages=['code_query'])
