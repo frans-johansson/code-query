afe46345ad8575c2ba20ba8ac4facbc8e84d9db0
Frans Johansson <fransjohansson98@gmail.com>
2022-01-01 14:09:27 +0100

    Implement the CodeQuery model as a LightningModule

--- src/code_query/model/model.py
+++ src/code_query/model/model.py
@@ -1,1 +1,137 @@
-# TODO: Implement
+"""
+Defines a Pytorch-Lightning module for setting up and training the CodeQuery model
+"""
+from argparse import ArgumentParser, Namespace
+from typing import Any, Tuple
+
+import torch
+from torch import nn
+from torch import optim
+import pytorch_lightning as pl
+
+from code_query.model.encoder import Encoder
+
+
+class CodeQuery(pl.LightningModule):
+    """
+    Represents the main `CodeQuery` model consisting of a pair of siamese encoder
+    networks which map codes and queries to a common latent space. The model then
+    trained using a simple triplet loss where positive code-query pairs are mapped
+    closer together in the latent space, and negative pairs are mapped further apart
+    """
+    @staticmethod
+    def add_argparse_args(parent_parser: ArgumentParser):
+        """
+        Add model specific arguments to a parent `ArgumentParser`
+        """
+        # Handle model args
+        parser = parent_parser.add_argument_group("CodeQuery")
+        parser.add_argument("--encoder_type", type=Encoder.Types)
+        parser.add_argument("--learning_rate", type=float, default=0.1)
+        parser.add_argument("--cq_h_dim", type=int, default=128)
+        parser.add_argument("--cq_dropout", type=float, default=0.1)
+        # Set up Encoder args
+        try:
+            temp_args, _ = parent_parser.parse_known_args()
+            parent_parser = Encoder.get_type(temp_args.encoder_type).add_argparse_args(parent_parser)
+        except:
+            raise ValueError("Please provide a value for the --encoder_type argument")
+        return parent_parser
+
+    def __init__(self, hparams: Namespace) -> None:
+        """
+        Set up a `CodeQuery` model for training
+
+        Args:
+            learning_rate (float): The initial learning rate for the
+                AdamW optimizer. Defaults to 0.1.
+            cq_h_dim (int): The dimensions of the final dense latent
+                space of the codes and queries. Defaults to 128.
+            cq_dropout (float): Dropout rate from the encoder step to
+                the final dense representation. Defaults to 0.1.
+            encoder_type (Encoder.Types): Name of the encoder type to use,
+                e.g. "nbow" or "bert".
+            encoder_args (kwargs): Additional keyword arguments required by
+                the encoder module.
+        """
+        super().__init__()
+        self.save_hyperparameters(hparams)
+        EncoderClass = Encoder.get_type(hparams.encoder_type)
+        self.encoder = EncoderClass(hparams)
+        self.fc = nn.LazyLinear(out_features=hparams.cq_h_dim)
+        self.bn = nn.LazyBatchNorm1d()
+        self.drop = nn.Dropout(p=hparams.cq_dropout)
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass used for inference. Computes the encoding of either
+        a tokenized code or query tensor.
+        
+        Args:
+            X: A tokenized code or query sequence 
+        """
+        encoded = self.encoder(X)
+        out = self.fc(encoded)
+        out = self.bn(out)
+        out = torch.sigmoid(out)
+        out = self.drop(out)
+        return out
+
+    def _encoded_pair(self, X: Any) -> Tuple[torch.Tensor]:
+        """
+        Encodes a data input of codes and queries and returns a tuple of the results
+        """
+        codes = X["code"]
+        queries = X["query"]
+
+        h_codes = self.forward(codes)
+        h_queries = self.forward(queries)
+        return (h_codes, h_queries)
+
+    def _training_loss(self, h_codes, h_queries) -> torch.Tensor:
+        """
+        Computes and returns the training and validation loss for an encoded pair
+        """
+        n = self.hparams.batch_size
+        mat = h_queries @ h_codes.T  # (queries, codes)
+        pos = mat.diag()  # True code-query pairings
+        off = mat.masked_select(~torch.eye(n, dtype=bool)).view(n, n - 1)
+        neg = off.exp().sum(dim=1)  # False code-query pairings
+        
+        loss = torch.log(torch.sum(pos / neg + 10e-9))
+        return -loss / n
+
+    def training_step(self, X: Any) -> torch.Tensor:
+        """
+        Performs a single trainig step and returns the loss
+
+        Args:
+            X ({"code": Tensor, "query": Tensor}): A dictionary of the tokenized
+                code and query input tensors of shape (B, L) where B is the batch size
+                and L is the length of each sequence
+        """
+        h_codes, h_queries = self._encoded_pair(X)
+        loss = self._training_loss(h_codes, h_queries)
+        return loss
+
+    def validation_step(self, X: Any, idx: int) -> torch.Tensor:
+        """
+        Performs a single validation step and returns the loss
+
+        Args:
+            X ({"code": Tensor, "query": Tensor}): A dictionary of the tokenized
+                code and query input tensors of shape (B, L) where B is the batch size
+                and L is the length of each sequence
+            idx (int): The index of the current validation data sample
+        """
+        h_codes, h_queries = self._encoded_pair(X)
+        loss = self._training_loss(h_codes, h_queries)
+        return loss
+
+    def configure_optimizers(self) -> None:
+        """
+        Sets up optimizers for the model training process configured with the
+        `learning_rate` hyperparameter
+        """
+        optimizer = optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+        return optimizer
