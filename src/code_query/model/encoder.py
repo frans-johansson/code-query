0238600da931bb2870d02c0e9be662d25e096a7b
Frans Johansson <fransjohansson98@gmail.com>
2022-01-06 00:34:43 +0100

    Implement BERT-like encoder

--- src/code_query/model/encoder.py
+++ src/code_query/model/encoder.py
@@ -8,8 +8,9 @@ from enum import Enum
 import torch
 from torch import nn
 import pytorch_lightning as pl
+from transformers import AutoModel, AutoConfig
 
-from code_query.config import TRAINING
+from code_query.config import MODELS, TRAINING
 
 
 class Encoder(ABC):
@@ -34,14 +35,8 @@ class Encoder(ABC):
         """
         if encoder_type == Encoder.Types.NBOW:
             return NbowEncoder
-        if encoder_type == Encoder.Types.BERT:
-            raise NotImplementedError()
-        if encoder_type == Encoder.Types.ROBERTA:
-            raise NotImplementedError()
-        if encoder_type == Encoder.Types.CODEBERT:
-            raise NotImplementedError()
-        if encoder_type == Encoder.Types.DISTILBERT:
-            raise NotImplementedError()
+        else:
+            return BertLikeEncoder
 
     @abstractstaticmethod
     def add_argparse_args(parent_parser: ArgumentParser) -> ArgumentParser:
@@ -105,6 +100,61 @@ class NbowEncoder(pl.LightningModule, Encoder):
             `encoding_dim` hyperparameter
         """
         embeddings = self.embed(X)  # (batch, seq, embedding)
+        nbow = torch.mean(embeddings, dim=1)  # (batch, embedding)
+        hidden = self.fc(nbow)
+        hidden = self.bn(hidden)
+        hidden = torch.tanh(hidden)
+        hidden = self.drop(hidden)
+        return hidden
+
+
+class BertLikeEncoder(pl.LightningModule, Encoder):
+    """
+    An encoder using a BERT-like embedding model
+    """
+    @staticmethod
+    def add_argparse_args(parent_parser: ArgumentParser) -> ArgumentParser:
+        """
+        Add encoder specific arguments to a parent `ArgumentParser`
+        """
+        parser = parent_parser.add_argument_group("NbowEncoder")
+        parser.add_argument("--encoding_dim", type=int, default=128)
+        parser.add_argument("--encoder_dropout", type=float, default=0.1)
+        return parent_parser
+
+    def __init__(self, hparams: Union[Dict[str, Any], Namespace]) -> None:
+        """
+        Sets up a BERT-like encoder for code and queries
+
+        Hyperparameters:
+            encoder_type (Encoder.Types): A string specifying the BERT variant to use.
+            encoding_dim (int): Size of the hidden dimensions for each sequence.
+                Defaults to 128.
+            encoder_dropout (float): Dropout rate. Defaults to 0.1.
+        """
+        super().__init__()
+        self.save_hyperparameters(hparams)
+        model_name = MODELS[self.hparams.encoder_type.value.upper()]
+        self.embed = AutoModel.from_pretrained(model_name)
+        self.fc = nn.Linear(
+            in_features=AutoConfig.from_pretrained(model_name).hidden_size,
+            out_features=self.hparams.encoding_dim
+        )
+        self.bn = nn.BatchNorm1d(self.hparams.encoding_dim)
+        self.drop = nn.Dropout(p=self.hparams.encoder_dropout)
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        """
+        Runs a forward pass through the encoder, producing a latent representation of the input
+
+        Args:
+            X (Tensor): A tensor of shape (B, 1, L) representing the input sequence
+                where B is the batch size and L is the sequence length of each sample
+
+        Returns: A tensor of shape (B, H) of latent representations, where H is set by the
+            `encoding_dim` hyperparameter
+        """
+        embeddings = self.embed(X.squeeze(1)).last_hidden_state  # (batch, seq, embedding)
         nbow = torch.mean(embeddings, dim=1)  # (batch, embedding)
         hidden = self.fc(nbow)
         hidden = self.bn(hidden)
