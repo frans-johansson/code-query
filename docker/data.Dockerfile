977e98100e1bf3b30246eee5eaa722885f2c0d41
Frans Johansson <fransjohansson98@gmail.com>
2022-01-23 20:52:32 +0100

    Clean up repository

--- docker/data.Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3.7.11
-ENV LANG=C.UTF-8 LC_ALL=C.UTF-8
-
-WORKDIR /home/dev
-
-CMD ["scripts/setup/data"]
--- scripts/download_and_process.py
+++
@@ -1,4 +0,0 @@
-from code_query.data import CSNetDataManager
-
-if __name__ == "__main__":
-    CSNetDataManager(code_lang="python", query_langs=None)
